548c2d68e089598580189129572fbc4b879e65cb
Andres Yepes <yepes07@gmail.com>
2018-07-09 21:57:52 -0500

    create Buttons and labels with StoryBoards

--- click_counter/ViewController.swift
+++ click_counter/ViewController.swift
@@ -11,60 +11,27 @@ import UIKit
 class ViewController: UIViewController {
     
     var count = 0
-    var label1: UILabel!
-    var label2: UILabel!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //label 1
-        let label1 = UILabel()
-        label1.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
-        label1.text = "0"
-        view.addSubview(label1)
-        self.label1 = label1
-        
-        //label 2
-        let label2 = UILabel()
-        label2.frame = CGRect(x: 210, y: 150, width: 60, height: 60)
-        label2.text = "0"
-        view.addSubview(label2)
-        self.label2 = label2
-        
-        //Increment button
-        let incrementButton = UIButton()
-        incrementButton.frame = CGRect(x: 150, y: 250, width: 150, height: 60)
-        incrementButton.setTitle("Increment", for: .normal)
-        incrementButton.setTitleColor(UIColor.blue, for: .normal)
-        view.addSubview(incrementButton)
-        
-        incrementButton.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControlEvents.touchUpInside)
-        incrementButton.addTarget(self, action: #selector(ViewController.changeBackgroundColor), for: UIControlEvents.touchUpInside)
-        
-        // Decrement Button
-        let decrementButton = UIButton()
-        decrementButton.frame = CGRect(x: 150, y: 350, width: 150, height: 60)
-        decrementButton.setTitle("Decrement", for: .normal)
-        decrementButton.setTitleColor(UIColor.red, for: .normal)
-        view.addSubview(decrementButton)
-        
-        decrementButton.addTarget(self, action: #selector(ViewController.decrementCount), for: UIControlEvents.touchUpInside)
-        decrementButton.addTarget(self, action: #selector(ViewController.changeBackgroundColor), for: UIControlEvents.touchUpInside)
     }
     
-    @objc func incrementCount(){
+    @IBAction func incrementCount(){
         self.count += 1
         self.label1.text = "\(self.count)"
         self.label2.text = "\(self.count)"
     }
     
-    @objc func decrementCount(){
+    @IBAction func decrementCount(){
         self.count -= 1
         self.label1.text = "\(self.count)"
         self.label2.text = "\(self.count)"
     }
     
-    @objc func changeBackgroundColor() {
+    @IBAction func changeBackgroundColor() {
         if self.count % 2 == 0 {
             view.backgroundColor = UIColor.white
         }
